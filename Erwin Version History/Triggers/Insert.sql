844ebd1b15dbd97bea3872a64921e3dd375a68a7
Adrian Legion <nlawliet6@gmail.com>
2022-12-15 15:37:38 -0500

    Final SQL Server Model(with keys and Triggers), DDL, BackUp File(.bak), Data Migration Scripts

--- Erwin Version History/Triggers/Insert.sql
+++ Erwin Version History/Triggers/Insert.sql
@@ -2,18 +2,8 @@ CREATE OR ALTER TRIGGER %ProcedureOwner.%TriggerName ON %TableOwner.%TableName
 %SQLServerWithOptions %Fire %Actions(",")
 %ProcedureProps
 AS
-/* Customized Erwin Builtin Trigger */
-/* %Actions(",") trigger on %TableOwner(.)%TableName */
-/* default body fox %ProcedureOwner(.)%TriggerName */
 
 BEGIN
-/*
-	Update Trigger with upfront intialization for the variables to be inserted into
-	the trigger history table
-*/
---
---  https://stackoverflow.com/questions/884334/tsql-try-catch-transaction-in-trigger
---
 
 set xact_abort OFF;
 
@@ -24,19 +14,7 @@ BEGIN TRY
     DECLARE 
     @DBAction CHAR(1)
     ,@IsDeleted CHAR(1)
-    -- ,@NoteDefault CHAR(200)
-    -- ,@Note CHAR(200)
-    -- ,@FinalNote CHAR(200)
     ,@NumberOfRows INT
-    -- ,@nullcnt INT
-    -- ,@validcnt INT
-    -- ,@errno INT
-    -- ,@severity INT
-    -- ,@state INT
-    -- ,@errmsg VARCHAR(255);
-    
-    --	The Trigger determines DBAction 
-    --  The only actions that we will deal with are Update and Delete.  Inserts are ignored
 
     DECLARE @id integer
     DECLARE @tnum integer
@@ -46,11 +24,6 @@ BEGIN TRY
         SET @DBAction = 'I'
         SET @IsDeleted = 'N'
 
-        -- SET @NoteDefault = 'Database Row Added. Transaction: 1. '
-
-        -- SELECT @id = %TableNameId, @Note = Note 
-        -- FROM Inserted
-
         SELECT @NumberOfRows = @@rowcount
 
         ----------------------------------
@@ -59,51 +32,20 @@ BEGIN TRY
         insert into Audit.%TableNameHistory
         (
             %ForEachColumn(Parent, ",") { %ColName }
-
             ,TransactionNumber
             ,IsDeleted
             ,DBAction
-
         )
         select
             %ForEachColumn(Parent, ", ") { %ColName }
-
             ,1
             ,@IsDeleted
             ,@DBAction
-
         from Inserted ORDER BY %TableNameId, SysStartTime
-
-        PRINT @id
-        -- PRINT(@Note)
-        -- PRINT(@NoteDefault)
-        -- SELECT @FinalNote = CONCAT(@NoteDefault, @Note)
-
-
-
-        -- UPDATE  tbl SET tbl.Note = @FinalNote FROM Audit.%TableNameHistory tbl INNER JOIN inserted i ON tbl.%TableNameId = i.%TableNameId ORDER BY tbl.TransactionNumber DESC
 
         --------------- END --------------
         ----------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     END;
-
-    
-
-
 END TRY
 BEGIN CATCH
    SELECT ERROR_MESSAGE() AS [Error Message]
@@ -117,6 +59,6 @@ END;
 
 %DBMSDelim
 
-ENABLE TRIGGER %ProcedureOwner(.)%TriggerName ON %TableOwner(.)%TableName
+ENABLE TRIGGER %ProcedureOwner.%TriggerName ON %TableOwner(.)%TableName
 
 %DBMSDelim
